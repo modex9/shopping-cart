dd8e1431a73d77e5581635caaade0e187b471d4d
modex9 <modestas.slivinskas@gmail.com>
2020-10-18 15:59:05 +0300

    abstracted some file handling logic

--- classes/FileHandler.php
+++ classes/FileHandler.php
@@ -1,10 +1,26 @@
 <?php 
 
 namespace Entity;
+use Traits;
 
-class FileHandler extends Entity
+abstract class FileHandler
 {
+    use Traits\Printable;
+
     public $filename = "input.txt";
+
+    public $columns_delimiter = ';';
+
+    protected $entity;
+
+    protected $num_columns;
+
+    public function __construct()
+    {
+        $this->eol = $this->getEol();
+        $this->entity = static::ENTITY;
+        $this->num_columns = static::NUM_COLUMNS;
+    }
 
     public function read()
     {
@@ -18,8 +34,31 @@ class FileHandler extends Entity
             
     }
 
-    public function write($content)
+    protected function write($content)
     {
         file_put_contents($this->filename, $content, FILE_APPEND | LOCK_EX);
+    }
+
+    protected function getLines()
+    {
+        $file_contents = $this->read();
+        if(!$file_contents)
+            return false;
+        $lines = explode("\r\n", $file_contents);
+        return $lines;
+    }
+
+    public function parseEntityLine($line, $file = true)
+    {
+        $entity_fields = explode($this->columns_delimiter, $line);
+        if(count($entity_fields) != $this->num_columns)
+        {
+            if($file)
+                $this->printLine("Failed reading {$this->entity} information at line {$line}. Check file formatting.");
+            else
+                $this->printLine("Failed to add {$this->entity}. Check your input formatting.");
+            return false;
+        }
+        return $entity_fields;
     }
 }
--- classes/ProductsFileHandler.php
+++ classes/ProductsFileHandler.php
@@ -4,25 +4,16 @@ namespace Entity;
 
 class ProductsFileHandler extends FileHandler
 {
-    public $columns_delimiter = ';';
-
     public $cart;
 
-    private const NUM_COLUMNS = 5;
+    protected const NUM_COLUMNS = 5;
 
-    private function getProductLines()
-    {
-        $file_contents = parent::read();
-        if(!$file_contents)
-            return false;
-        $products = explode("\r\n", $file_contents);
-        return $products;
-    }
+    protected const ENTITY = 'product';
 
     // Returns array of Products
     public function getProducts()
     {
-        $product_lines = $this->getProductLines();
+        $product_lines = $this->getLines();
         if(!$product_lines || (is_array($product_lines) && count($product_lines) == 0))
             return false;
 
@@ -30,7 +21,7 @@ class ProductsFileHandler extends FileHandler
         $validator = new Validator();
         foreach($product_lines as $i => $product)
         {
-            $product_fields = $this->parseProductLine($product);
+            $product_fields = $this->parseEntityLine($product);
             if(!$product_fields)
                 continue;
 
@@ -48,27 +39,12 @@ class ProductsFileHandler extends FileHandler
         return $products;
     }
 
-    private function parseProductLine($line, $file = true)
-    {
-        $product_fields = explode($this->columns_delimiter, $line);
-        if(count($product_fields) != self::NUM_COLUMNS)
-        {
-            if($file)
-                $this->printLine("Failed reading product information at line {$line}. Check file formatting.");
-            else
-                $this->printLine("Cart update failed. Check your input formatting.");
-            return false;
-        }
-        return $product_fields;
-    }
-
     public function write($content)
     {
         $eol = $this->eol;
-        $product_fields = $this->parseProductLine($content, false);
+        $product_fields = $this->parseEntityLine($content, false);
         if($product_fields)
         {
-
             $product = $this->createProduct($product_fields);
             if($product->quantity == 0)
             {
